40056e2f81ca719b8a48c0e825976938bf9d48ba
Amrita Pandey <amritait3009@gmail.com>
2023-07-18 19:57:05 +0530

    Added first version code

---
+++ src/common/header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+import { Component } from "react";
+
+class Header extends Component {
+    constructor(){
+      super();  
+    }
+
+    render() {
+        return (
+          <div>
+            <header className="header"/>
+              
+            </div>
+        );
+    }
+}
